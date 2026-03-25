5b150c6be4a4e02241fd29ede8c1371dbd066c86
Dennis Heller <dennis.heller@geovision.de>
2026-03-25 18:30:23 +0100

    Add copy button to readonly TextBox

--- ModernWpf/Controls/Primitives/TextBoxHelper.cs
+++ ModernWpf/Controls/Primitives/TextBoxHelper.cs
@@ -1,4 +1,4 @@
-﻿using System.Windows;
+using System.Windows;
 using System.Windows.Controls;
 
 namespace ModernWpf.Controls.Primitives
@@ -6,7 +6,8 @@ namespace ModernWpf.Controls.Primitives
     public static class TextBoxHelper
     {
         private const string ButtonStatesGroup = "ButtonStates";
-        private const string ButtonVisibleState = "ButtonVisible";
+        private const string DeleteButtonVisibleState = "DeleteButtonVisible";
+        private const string CopyButtonVisibleState = "CopyButtonVisible";
         private const string ButtonCollapsedState = "ButtonCollapsed";
 
         #region IsEnabled
@@ -30,7 +31,7 @@ namespace ModernWpf.Controls.Primitives
 
         private static void OnIsEnabledChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
         {
-            var textBox = (TextBox)d;
+            TextBox textBox = (TextBox)d;
 
             if ((bool)e.NewValue)
             {
@@ -99,7 +100,7 @@ namespace ModernWpf.Controls.Primitives
 
         private static void OnIsDeleteButtonChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
         {
-            var button = (Button)d;
+            Button button = (Button)d;
 
             if ((bool)e.OldValue)
             {
@@ -135,35 +136,114 @@ namespace ModernWpf.Controls.Primitives
 
         private static void OnIsDeleteButtonVisibleChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
         {
-            UpdateVisualStates((TextBox)d, (bool)e.NewValue);
+            UpdateVisualStates((TextBox)d);
+        }
+
+        #endregion
+
+        #region IsCopyButton
+
+        public static bool GetIsCopyButton(Button button)
+        {
+            return (bool)button.GetValue(IsCopyButtonProperty);
+        }
+
+        public static void SetIsCopyButton(Button button, bool value)
+        {
+            button.SetValue(IsCopyButtonProperty, value);
+        }
+
+        public static readonly DependencyProperty IsCopyButtonProperty =
+            DependencyProperty.RegisterAttached(
+                "IsCopyButton",
+                typeof(bool),
+                typeof(TextBoxHelper),
+                new PropertyMetadata(OnIsCopyButtonChanged));
+
+        private static void OnIsCopyButtonChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            Button button = (Button)d;
+
+            if ((bool)e.OldValue)
+            {
+                button.Click -= OnCopyButtonClick;
+            }
+
+            if ((bool)e.NewValue)
+            {
+                button.Click += OnCopyButtonClick;
+            }
+        }
+
+        #endregion
+
+        #region IsCopyButtonVisible
+
+        public static readonly DependencyProperty IsCopyButtonVisibleProperty =
+            DependencyProperty.RegisterAttached(
+                "IsCopyButtonVisible",
+                typeof(bool),
+                typeof(TextBoxHelper),
+                new PropertyMetadata(OnIsCopyButtonVisibleChanged));
+
+        public static bool GetIsCopyButtonVisible(TextBox textBox)
+        {
+            return (bool)textBox.GetValue(IsCopyButtonVisibleProperty);
+        }
+
+        public static void SetIsCopyButtonVisible(TextBox textBox, bool value)
+        {
+            textBox.SetValue(IsCopyButtonVisibleProperty, value);
+        }
+
+        private static void OnIsCopyButtonVisibleChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            // Templates/control styles decide how to present the copy button.
+            // Keep parity with delete button visible change handler so consumers can use this property in triggers.
+            UpdateVisualStates((TextBox)d);
         }
 
         #endregion
 
         private static void OnLoaded(object sender, RoutedEventArgs e)
         {
-            var textBox = (TextBox)sender;
-            UpdateVisualStates(textBox, GetIsDeleteButtonVisible(textBox));
+            TextBox textBox = (TextBox)sender;
+            UpdateVisualStates(textBox);
         }
 
         private static void OnTextChanged(object sender, TextChangedEventArgs e)
         {
-            var textBox = (TextBox)sender;
+            TextBox textBox = (TextBox)sender;
             UpdateHasText(textBox);
         }
 
         private static void OnDeleteButtonClick(object sender, RoutedEventArgs e)
         {
-            var button = (Button)sender;
+            Button button = (Button)sender;
             if (button.TemplatedParent is TextBox textBox)
             {
                 textBox.SetCurrentValue(TextBox.TextProperty, null);
             }
         }
 
-        private static void UpdateVisualStates(TextBox textBox, bool isDeleteButtonVisible)
+        private static void OnCopyButtonClick(object sender, RoutedEventArgs e)
         {
-            VisualStateManager.GoToState(textBox, isDeleteButtonVisible ? ButtonVisibleState : ButtonCollapsedState, true);
+            Button button = (Button)sender;
+            if (button.TemplatedParent is TextBox textBox)
+            {
+                Clipboard.SetText(textBox.Text ?? string.Empty);
+            }
+        }
+
+        private static void UpdateVisualStates(TextBox textBox)
+        {
+            VisualStateManager.GoToState(textBox,
+                                         GetIsDeleteButtonVisible(textBox)
+                                             ? DeleteButtonVisibleState
+                                             : GetIsCopyButtonVisible(textBox)
+                                                 ? CopyButtonVisibleState
+                                                 : ButtonCollapsedState,
+                                         true);
         }
     }
 }
